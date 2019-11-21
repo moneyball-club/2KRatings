80b63db467c015339d0d90ec3646d4412b350b99
kiwilvio <qehez11@gmail.com>
2019-11-21 00:42:21 -0800

    Add files via upload

--- 2kstats.R
+++ 2kstats.R
@@ -147,7 +147,7 @@ bref_players_stats(seasons = 2018:2020, tables = c("advanced", "totals", "per_mi
                                  assign_to_environment = TRUE, widen_data = TRUE, join_data = TRUE,
                                  return_message = TRUE)
 
-
+#### PREP ADVANCED
 class(dataBREFPlayerAdvanced)
 head(dataBREFPlayerAdvanced)
 View(dataBREFPlayerAdvanced)
@@ -158,6 +158,8 @@ names(train_adv)
 names(train_adv)[c(1, 5, 17)] <- c("Player.Name", "Year", "team")
 train_adv_test <- train_adv[, -c(2,4,6,7,8,9,10,11,12,13,14,42)]
 View(train_adv_test)
+
+#### PREP
 
 
 #data merge
@@ -206,7 +208,7 @@ preProcValues <- preProcess(filteredDescr[,-c(2,31)], method = c("center", "scal
 preProcValues
 
 transformed<-predict(preProcValues, newdata = filteredDescr[,-c(2,31)])
-ready_data<-data.frame(merged_data$Rating,merged_data$Year,transformed)
+ready_data<-data.frame(merged_data$Rating,merged_data$Year,transformed[,-15])
 
 train_data <- subset(ready_data, merged_data.Year==2018 | merged_data.Year==2019)
 test_data <- subset(ready_data, merged_data.Year == 2020)
@@ -222,25 +224,144 @@ brnn_model <- caret::train(y=train_data$merged_data.Rating,
 brnn_model
 
 # Bayesian Regularized Neural Networks 
-# 
-# 620 samples
-# 16 predictor
-# 
-# No pre-processing
-# Resampling: Cross-Validated (10 fold, repeated 10 times) 
-# Summary of sample sizes: 557, 559, 559, 557, 557, 556, ... 
-# Resampling results across tuning parameters:
-#   
-#   neurons  RMSE        Rsquared   MAE       
-# 1        0.75400427  0.9875983  0.51789637
-# 2        0.07473078  0.9998764  0.05487381
-# 3        0.06659471  0.9999064  0.04542111
+# neurons  RMSE      Rsquared   MAE     
+# 1        2.537176  0.8504387  1.901824
+# 2        2.489781  0.8555577  1.859597
+# 3        2.475014  0.8576149  1.854231
 # 
 # RMSE was used to select the optimal model using the smallest value.
 # The final value used for the model was neurons = 3.
 
 brnn_predictions <- predict(brnn_model, newdata = test_data[,-1])
-results_data <- data.frame(brnn_predictions, test_data)
+results_data_brnn <- data.frame(brnn_predictions, test_data)
+results_data_brnn$errors <- abs(results_data_brnn$merged_data.Rating - results_data_brnn$brnn_predictions)
+sum(results_data_brnn$errors)
+#1530
 
-write.csv(train_data, file = "train_data.csv")
-write.csv(results_data, file = "results.csv")
+# write.csv(train_data, file = "train_data.csv")
+# write.csv(results_data, file = "results.csv")
+
+xgb_model <- caret::train(y=train_data$merged_data.Rating, 
+                           x=train_data[,-1],
+                           trControl=fitControl,
+                           method='xgbTree') 
+xgb_model
+
+# 0.4  3          0.8               1.00       150      2.610294  0.8456342  1.991517
+# 
+# Tuning parameter 'gamma' was held constant at a value of 0
+# Tuning parameter 'min_child_weight' was
+# held constant at a value of 1
+# RMSE was used to select the optimal model using the smallest value.
+# The final values used for the model were nrounds = 100, max_depth = 2, eta = 0.3, gamma =
+#   0, colsample_bytree = 0.8, min_child_weight = 1 and subsample = 1.
+
+xgb_predictions <- predict(xgb_model, newdata = test_data[,-1])
+results_data_xgb <- data.frame(xgb_predictions, test_data)
+results_data_xgb$errors <- abs(results_data_xgb$merged_data.Rating - results_data_xgb$xgb_predictions)
+sum(results_data_xgb$errors)
+#1452
+
+lm_model <- caret::train(y=train_data$merged_data.Rating, 
+                          x=train_data[,-1],
+                          trControl=fitControl,
+                          method='lm') 
+lm_model
+
+# Resampling results:
+#   
+#   RMSE      Rsquared   MAE     
+# 2.894195  0.8079294  2.145151
+# 
+# Tuning parameter 'intercept' was held constant at a value of TRUE
+
+lm_predictions <- predict(lm_model, newdata = test_data[,-1])
+results_data_lm <- data.frame(lm_predictions, test_data)
+results_data_lm$errors <- abs(results_data_lm$merged_data.Rating - results_data_lm$lm_predictions)
+sum(results_data_lm$errors)
+#1408
+
+rf_model <- caret::train(y=train_data$merged_data.Rating, 
+                         x=train_data[,-1],
+                         trControl=fitControl,
+                         method='rf') 
+rf_model
+# mtry  RMSE      Rsquared   MAE     
+# 2    2.619232  0.8633921  1.995339
+# 8    2.466305  0.8590897  1.872052
+# 15    2.530999  0.8485010  1.894308
+# 
+# RMSE was used to select the optimal model using the smallest value.
+# The final value used for the model was mtry = 8.
+
+rf_predictions <- predict(rf_model, newdata = test_data[,-1])
+results_data_rf <- data.frame(rf_predictions, test_data)
+results_data_rf$errors <- abs(results_data_rf$merged_data.Rating - results_data_rf$rf_predictions)
+sum(results_data_rf$errors)
+#1504
+
+gam_model <- caret::train(y=train_data$merged_data.Rating, 
+                         x=train_data[,-1],
+                         trControl=fitControl,
+                         method='gam') 
+gam_model
+
+# select  RMSE      Rsquared   MAE     
+# FALSE   2.915075  0.8128504  2.003567
+# TRUE   2.837884  0.8186052  1.989842
+
+gam_predictions <- predict(gam_model, newdata = test_data[,-1])
+results_data_gam <- data.frame(gam_predictions, test_data)
+results_data_gam$errors <- abs(results_data_gam$merged_data.Rating - results_data_gam$gam_predictions)
+sum(results_data_gam$errors)
+#1360
+
+gbm_model <- caret::train(y=train_data$merged_data.Rating, 
+                          x=train_data[,-1],
+                          trControl=fitControl,
+                          method='gbm') 
+gbm_model
+# interaction.depth  n.trees  RMSE      Rsquared   MAE     
+# 1                   50      2.853920  0.8220613  2.160842
+# 1                  100      2.620740  0.8401669  1.976787
+# 1                  150      2.599260  0.8426921  1.952410
+# 2                   50      2.604428  0.8428876  1.953885
+# 2                  100      2.542038  0.8492274  1.905408
+# 2                  150      2.538590  0.8497147  1.903887
+# 3                   50      2.521607  0.8523706  1.902816
+# 3                  100      2.495766  0.8551177  1.876184
+# 3                  150      2.486554  0.8563681  1.871741
+
+gbm_predictions <- predict(gbm_model, newdata = test_data[,-1])
+results_data_gbm <- data.frame(gbm_predictions, test_data)
+results_data_gbm$errors <- abs(results_data_gbm$merged_data.Rating - results_data_gbm$gbm_predictions)
+sum(results_data_gbm$errors)
+#1480
+
+###### BEST MODEL
+gam_model <- caret::train(y=train_data$merged_data.Rating, 
+                          x=train_data[,-1],
+                          trControl=fitControl,
+                          method='gam') 
+gam_model
+
+# select  RMSE      Rsquared   MAE     
+# FALSE   2.915075  0.8128504  2.003567
+# TRUE   2.837884  0.8186052  1.989842
+
+gam_predictions <- predict(gam_model, newdata = test_data[,-1])
+results_data_gam <- data.frame(gam_predictions, test_data)
+results_data_gam$errors <- abs(results_data_gam$merged_data.Rating - results_data_gam$gam_predictions)
+results_data_gam <- data.frame(subset(merged_data[,c(1,2)], Year == 2020), results_data_gam)
+sum(results_data_gam$errors)
+
+gam_imp <- varImp(gam_model, scale = FALSE)
+plot(gam_imp)
+
+p <- ggplot(results_data_gam, aes(x = merged_data.Rating, y = gam_predictions, text = Player.Name)) +
+  geom_point() 
+  # geom_text(aes(label=ifelse(gam_predictions>80,as.character(Player.Name), "")), hjust=1, vjust=-.4, 
+  #           size = 3)
+p <- ggplotly(p)
+
+
